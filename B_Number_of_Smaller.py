9610fc793e804797d6f63a8b696301bee4229ce6
hawariyaw paulos <hawipaul@gmail.com>
2026-03-12 21:10:54 +0300

    smaller

--- B_Number_of_Smaller.py
+++ B_Number_of_Smaller.py
@@ -4,9 +4,9 @@ arr2 = list(map(int, input().split()))
 
 
 ans = []
-for i in range(len(arr2)):
-    left = 0
-    while left < m and arr2[i] > arr1[left]:
+left = 0
+for i in range(len(arr2)):  
+    while left < n and arr2[i] > arr1[left]:
             left += 1
     ans.append(left)
 print(*ans)
